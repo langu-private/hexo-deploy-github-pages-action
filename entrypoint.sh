10fe906af485a908d74803c0e424a0bd39133951
langu <30767010+langu-private@users.noreply.github.com>
2022-12-18 15:59:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,6 +62,7 @@ echo ">>> Config git ..."
 git init
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global --add safe.directory /github/workspace/public
 git remote add origin "${REPOSITORY_PATH}"
 
 git checkout --orphan $TARGET_BRANCH
